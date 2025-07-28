ac3e03aeb7523176cd9a7dc937aac0195fe6388b
keychera <kyogatama@gmail.com>
2025-07-29 04:58:50 +0900

    leva/monitor

--- src/engine/start.cljs
+++ src/engine/start.cljs
@@ -13,15 +13,16 @@
 
 (defonce !panel-atom
   (r/atom
-   {:crop? {:value false}
-    :frame {:value 0 :step 1 :min 0 :max 47}
-    :color {:r 200 :g 120 :b 120}
-    :point {:x 0 :y 0}}))
+   {:pressed-key {:value "no key" :render (constantly false)}
+    :crop?       {:value true}
+    :frame       {:value 0 :step 1 :min 0 :max 47}
+    :color       {:r 200 :g 120 :b 120}
+    :position    {:x 0 :y 0}}))
 
 (defmulti on-leva-change (fn [k _old _new] k))
 
 (defmethod on-leva-change :crop? [_ _ new']
- (swap! session/session* o/insert ::session/leva-spritesheet ::session/crop? new'))
+  (swap! session/session* o/insert ::session/leva-spritesheet ::session/crop? new'))
 
 (defmethod on-leva-change :frame [_ _ new']
   (swap! session/session* o/insert ::session/leva-spritesheet ::session/frame new'))
@@ -31,6 +32,9 @@
 
 (defmethod on-leva-change :point [_ _ {:keys [x y]}]
   (swap! session/session* o/insert ::session/leva-point {::session/x x ::session/y y}))
+
+(defmethod on-leva-change :default [k old' new']
+  (println k old' new'))
 
 (defn panel-watcher [_ _ old' new']
   (let [[removed added _common] (data/diff old' new')]
@@ -47,6 +51,10 @@
     (js/requestAnimationFrame
      (fn [ts]
        (let [ts (msec->sec ts)]
+         (try
+           (let [{:keys [x y]} (first (o/query-all @session/session* ::session/sprite-esse))]
+             (swap! !panel-atom assoc :position {:x x :y y}))
+           (catch js/Error e (println e)))
          (game-loop (assoc game
                            :delta-time (- ts (:total-time game))
                            :total-time ts)))))))
@@ -70,7 +78,6 @@
   #_(events/listen js/window "mouseup"
                    (fn [event]
                      (swap! engine/*state assoc :mouse-button nil))))
-
 (defn keycode->keyname [keycode]
   (condp = keycode
     37 :left
@@ -83,11 +90,13 @@
   (events/listen js/window "keydown"
                  (fn [event]
                    (when-let [keyname (keycode->keyname (.-keyCode event))]
+                     (swap! !panel-atom assoc :pressed-key (str keyname))
                      (swap! session/session* o/insert keyname ::session/pressed-key ::session/keydown))))
   (events/listen js/window "keyup"
                  (fn [event]
                    (when-let [keyname (keycode->keyname (.-keyCode event))]
                      (swap! session/session* o/insert keyname ::session/pressed-key ::session/keyup)))))
+
 
 (defn resize [context]
   (let [display-width context.canvas.clientWidth
@@ -109,8 +118,10 @@
     (.observe observer canvas)))
 
 ;; leva
-(defn main-panel [] 
-  [leva/Controls {:atom !panel-atom}])
+(defn main-panel []
+  [leva/Controls
+   {:atom !panel-atom
+    :schema {"last key" (leva/monitor (fn [] (:pressed-key @!panel-atom)) {})}}])
 
 (defonce root (delay (rdomc/create-root (.getElementById js/document "app"))))
 
